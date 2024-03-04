419cc4603c3d756da3455989e19e123a431981bd
jerome <jerome.michael@careplix.com>
2024-03-04 22:49:11 +0530

    access user from token

--- app/routers/post.py
+++ app/routers/post.py
@@ -9,15 +9,16 @@ router = APIRouter(prefix="/posts",tags=["Posts"])
 
 
 @router.get("/",response_model=List[schemas.PostResponse])
-def get_posts(db: Session = Depends(get_db),user_id : int =Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db),current_user : int =Depends(oauth2.get_current_user)):
     """ Get all posts request """
+    print(current_user.email)
     posts = db.query(models.Post).all()
     return posts
 
 @router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.PostResponse)
-def create_posts(post : schemas.PostCreate,db: Session = Depends(get_db),user_id : int =Depends(oauth2.get_current_user)):
+def create_posts(post : schemas.PostCreate,db: Session = Depends(get_db),current_user : int =Depends(oauth2.get_current_user)):
     """ Create post request """
-    print(user_id)
+    print(current_user.email)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -25,7 +26,7 @@ def create_posts(post : schemas.PostCreate,db: Session = Depends(get_db),user_id
     return new_post
 
 @router.get("/{id}",response_model=schemas.PostResponse)
-def get_post(id: int,db: Session = Depends(get_db),user_id : int =Depends(oauth2.get_current_user)):
+def get_post(id: int,db: Session = Depends(get_db),current_user : int =Depends(oauth2.get_current_user)):
     """ Get post by id request """
     post =db.query(models.Post).filter(models.Post.id==id).first()
     if not post:
@@ -33,7 +34,7 @@ def get_post(id: int,db: Session = Depends(get_db),user_id : int =Depends(oauth2
     return post
 
 @router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)  
-def delete_post(id: int,db: Session = Depends(get_db),user_id : int =Depends(oauth2.get_current_user)):
+def delete_post(id: int,db: Session = Depends(get_db),current_user : int =Depends(oauth2.get_current_user)):
     """ Delete post request """
     post_query = db.query(models.Post).filter(models.Post.id==id)
     if post_query.first() == None:
@@ -43,7 +44,7 @@ def delete_post(id: int,db: Session = Depends(get_db),user_id : int =Depends(oau
     return Response(status_code=status.HTTP_204_NO_CONTENT)
  
 @router.put("/{id}",response_model=schemas.PostResponse)
-def update_post(id:int,updated_post:schemas.PostCreate,db: Session = Depends(get_db),user_id : int =Depends(oauth2.get_current_user)):
+def update_post(id:int,updated_post:schemas.PostCreate,db: Session = Depends(get_db),current_user : int =Depends(oauth2.get_current_user)):
     """ Update post request """
     post_query = db.query(models.Post).filter(models.Post.id==id)
     post = post_query.first()
